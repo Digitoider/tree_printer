b6b3549ec27f5f339625876e39b602a9006272d4
Alexander <alexander@alexander.brjakin>
2018-08-30 07:07:58 +0300

    Initialize gem. Build basic structure

---
+++ lib/tree_printer/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module TreePrinter
+  VERSION = '0.1.0'
+end
---
+++ spec/tree_printer_spec.rb
@@ -0,0 +1,5 @@
+RSpec.describe TreePrinter do
+  it 'has a version number' do
+    expect(TreePrinter::VERSION).not_to be nil
+  end
+end
